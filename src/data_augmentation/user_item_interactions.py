a28b7b08a16df09f9e20f7608e5d8bf463272d0d
imed DRIOUA <=>
2024-03-26 23:25:09 +0100

    item attributes augmenttation

--- src/data_augmentation/user_item_interactions.py
+++ src/data_augmentation/user_item_interactions.py
@@ -5,6 +5,8 @@ from langchain_core.messages import HumanMessage
 
 def llm_user_item_interaction(user_history,candidates,llm):
     """
+    function that defines the prompt for the user-item interactions data augmentation.
+
     :param user_history: history of books read by user
     :type user_history: str
     :param candidates: a list of candidates books to be recommended
@@ -12,6 +14,7 @@ def llm_user_item_interaction(user_history,candidates,llm):
     :param llm: LLM model
     :type llm: LLM
     :return: LLM response
+    :rtype: str
     """
     prompt = f"""The user has read the following books,that each book with [index] title, year, genres. :
             {user_history}
