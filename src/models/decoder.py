3a2b2138d56dcc91879c445d7f4beea54882d83c
Boufafa Lamis <il_boufafa@esi.dz>
2024-03-29 17:31:08 +0100

    ading decoder

---
+++ src/models/decoder.py
@@ -0,0 +1,26 @@
+import torch
+import torch.nn as nn
+
+class Decoder(nn.Module):
+    def __init__(self, feat_size, embed_size=64):
+        super(Decoder, self).__init__()
+        self.feat_size = feat_size
+        self.user_net = nn.Sequential(
+            nn.Linear(embed_size, int(self.feat_size)),
+            nn.LeakyReLU(True),
+        )
+
+        self.item_net = nn.Sequential(
+            nn.Linear(embed_size, int(self.feat_size)),
+            nn.LeakyReLU(True),
+        )
+
+    def forward(self, user, item):
+        user_output = self.user_net(user.float())
+        tensor_list = []
+        keys = item.keys()
+        for _,value in enumerate(keys):
+            tensor_list.append(item[value])
+        item_tensor = torch.stack(tensor_list)
+        item_output = self.item_net(item_tensor.float())
+        return user_output, item_output
