f27dc0dc96cdea1d9391ca8a4cf4f30a52ea63f9
Boufafa Lamis <il_boufafa@esi.dz>
2024-03-29 23:43:27 +0100

    adding doc directory

---
+++ doc/source/conf.py
@@ -0,0 +1,30 @@
+import sys
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+sys.path.insert(0, '../../src/metrics.py')
+sys.path.insert(0, '../../src/train.py')
+project = 'ppd'
+copyright = '2024, lamis imed'
+author = 'lamis imed'
+release = '0.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [ 'sphinx.ext.autodoc', 'sphinx.ext.viewcode' ]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'fr'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
