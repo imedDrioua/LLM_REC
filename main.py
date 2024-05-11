b4fb415f830bdf1cd1b72d995c521c52317054d3
imed DRIOUA <=>
2024-05-11 15:58:22 +0200

    comments

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ from src.train import Trainer
 import torch
 import numpy as np
 import random
-
+# set the device
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-
+# set the seed for reproducibility
 def set_seed(seed):
     """
     Set the seed for reproducibility
@@ -21,7 +21,7 @@ def set_seed(seed):
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
 
-
+# main script
 if __name__ == "__main__":
     dataset_name = "books"
     set_seed(2022)
@@ -33,13 +33,13 @@ if __name__ == "__main__":
                     interactions=dataset.get_dataset("interactions"),
                     interactions_t=dataset.get_dataset("interactions_T"),
                     image_embeddings_data=dataset.get_dataset("images"),
-                    text_embeddings_data=dataset.get_dataset("text"), embed_size=64, n_layers=3,
+                    text_embeddings_data=dataset.get_dataset("text"), embed_size=128, n_layers=3,
                     user_profiles_data=dataset.get_dataset("user_profiles"),
                     book_attributes_data=dataset.get_dataset("books_attributes"))
     model.to(device)
 
     # load the trainer
-    trainer = Trainer(model=model, dataset=dataset, lr=0.0002)
+    trainer = Trainer(model=model, dataset=dataset, lr=0.005)
 
     # train the model
     trainer.train(epochs=10, batch_size=1024)
